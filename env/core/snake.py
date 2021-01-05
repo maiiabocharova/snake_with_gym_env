ab0b492dcf94bbbd9ad2aae79f3277a46c523e47
Maiia Bocharova <71256026+maiiabocharova@users.noreply.github.com>
2021-01-05 18:48:46 +0200

    Add files via upload

---
+++ env/core/snake.py
@@ -0,0 +1,42 @@
+import numpy as np
+from settings.constants import DIRECTIONS, SNAKE_BLOCK
+
+class Snake:
+    def __init__(self, head_position, direction_index, length):
+        """
+        @param head_position: tuple
+        @param direction_index: int
+        @param length: int
+        """
+        # Information snake need to know to make the move
+        self.snake_block = SNAKE_BLOCK
+        self.current_direction_index = direction_index
+                # Alive identifier
+        self.alive = True
+        # Place the snake
+        self.blocks = [head_position]
+        current_position = np.array(head_position)
+        for i in range(1, length):
+            # Direction inverse of moving
+            current_position = current_position - DIRECTIONS[self.current_direction_index]
+            self.blocks.append(tuple(current_position))
+
+    def step(self, action):
+        """
+        @param action: int
+        @param return: tuple, tuple
+        """
+        # Check if action can be performed (do nothing if in the same direction or opposite)
+        if np.abs(self.current_direction_index - action)!=2:
+            self.current_direction_index = action
+            # Remove tail
+        tail = self.blocks[-1]
+        self.blocks.pop(-1)
+        # Check new head
+        if len(self.blocks) != 0:
+            new_head = self.blocks[0] + DIRECTIONS[self.current_direction_index]
+        elif len(self.blocks) == 0:
+            new_head = tail + DIRECTIONS[self.current_direction_index]
+        # Add new head
+        self.blocks = [tuple(new_head)] + self.blocks
+        return tuple(new_head), tail
